46802c34addfb4ba479d993b8d9158ff17e4beb5
user <borvathcs@gmail.com>
2026-04-27 01:18:05 -0400

    Re-enabled Scrutinizer with only inconsistent regions showing

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use sesame_build::{Options, SesameBuilder};
 
 fn main() {
-    let mut builder = SesameBuilder::new(Options::new().verbose(true)).unwrap();
-    // builder.scrutinizer();
+    let mut builder = SesameBuilder::new(Options::new().verbose(false)).unwrap();
+    builder.scrutinizer();
     builder.linter();
 }
